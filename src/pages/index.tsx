fd750ae83b10405d0f498d4c334efdb1a0aa8435
Andrew Simms <simms.andrew@gmail.com>
2019-02-26 21:06:31 -0700

    Feature: Enable SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,21 +33,24 @@ const Description = (props: DescriptionProps) => (
 );
 
 const IndexPage = () => (
-  <Layout>
-    <h1 style={{ ...headerStyle, width: "100%", textAlign: "center" }}>
-      Hello, I'm Andrew.
-    </h1>
-    <MyCenterImage path={Me} />
-    <Description>
-      I am a programmer, photographer, guitar player, and maker currently living
-      in Golden, Colorado.
-    </Description>
-    <p>No go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <>
+    <SEO title="Test" />
+    <Layout>
+      <h1 style={{ ...headerStyle, width: "100%", textAlign: "center" }}>
+        Hello, I'm Andrew.
+      </h1>
+      <MyCenterImage path={Me} />
+      <Description>
+        I am a programmer, photographer, guitar player, and maker currently
+        living in Golden, Colorado.
+      </Description>
+      <p>No go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </>
 );
 
 export default IndexPage;
