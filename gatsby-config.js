f11f10c898fdb7beb33ffb8d69d5c9f62e5ed1cf
Andrew Simms <simms.andrew@gmail.com>
2019-02-26 20:26:36 -0700

    Content: Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Andrew Simms`,
+    description: `Programmer, photographer, guitar player and maker currently living in Golden, Colorado.`,
+    author: `@andrewdsimms`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
